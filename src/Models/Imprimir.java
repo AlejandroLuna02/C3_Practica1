fab8fb4638967d13aba32f0668f8fd8a1d4a8676
Alejandro Guillen <chusitoluna2002gmail.com>
2022-07-09 23:15:23 -0500

    Initial commit

---
+++ src/Models/Imprimir.java
@@ -0,0 +1,11 @@
+package Models;
+
+public class Imprimir {
+
+    public void resultado(int suma){
+        System.out.println("\n----------------------");
+        System.out.println("El resultado de la suma es:" + suma);
+        System.out.println("----------------------");
+
+    }
+}
